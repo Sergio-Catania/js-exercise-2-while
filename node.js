9cbee0b05197bb5d3eec0cbfcd616d17c789d7e7
Sergio Catania <sergiocatania1@gmail.com>
2023-10-25 14:07:33 +0200

    es.1

---
+++ node.js
@@ -0,0 +1,17 @@
+//es.1
+
+
+let prompt1 = prompt("Scrivi una frase");
+let prompt2 = prompt("Scegli una parola da cambiare");
+let prompt3 = prompt("Scegli un altra parola");
+
+let fraseCambiata = prompt1.replaceAll(prompt2, prompt3);
+
+console.log(fraseCambiata);
+
+
+Sostituire parole
+
+
+
+//es.2
